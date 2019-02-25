d20c55a25188bd373560966d5a8c8ce9830d611f
dhansenaz <dhansen@allthingsdot.com>
2019-02-25 07:34:53 -0800

    getseason function added icons and display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const SeasonDisplay = (props) => {
-    console.log(props.lat)
-    return(
-        <div>Season Display</div>
-    )
-}
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
 
-export default SeasonDisplay
+const SeasonDisplay = props => {
+  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+  return (
+    <div>
+        <i className={`${icon} icon`} />
+      <h1>
+        {season === "winter" ? "Burr it is chilly" : "Lets hit the beach"}
+      </h1>
+      <i className={`${icon} icon`} />
+      
+    </div>
+  );
+};
+
+export default SeasonDisplay;
